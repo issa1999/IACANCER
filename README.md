321c7734a0543723737271bc0bee3023f58187fc
firasbouali11 <firas.bouali11@gmail.com>
2021-04-28 11:32:14 +0100

    final touches

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IACANCER
 un site web qui a classifier des images des organes et la detection de types de cancer
+
+
+create a folder called models if not exists and place the models there
+https://mega.nz/file/49BkVZCS#oT6Aer20xLZBkmiSoGsbRSCYBY8JDeFco3gHN8kXN7Q
--- server.py
+++ server.py
@@ -5,14 +5,25 @@ import os
 import numpy as np
 from flask_cors import CORS
 
+BASEDIR = "/".join(os.path.abspath(__file__).split("/")[:-1])
 app = Flask(__name__)
 CORS(app)
 
-def initModels(classification=None,severity=None):
-    global typeCancerModel
-    global severityCancerModel
-    typeCancerModel = load_model(classification)
-    severityCancerModel = load_model(severity)
+typeCancerModel = load_model(os.path.join(BASEDIR,"models/modelTypeClassification.h5"))
+severityCancerModel = load_model(os.path.join(BASEDIR,"models/modelSeverityClassification.h5"))
+print("models loaded")
+
+types = ['melanoma',
+ 'squamous cell carcinoma',
+ 'actinic keratosis',
+ 'dermatofibroma',
+ 'pigmented benign keratosis',
+ 'seborrheic keratosis',
+ 'basal cell carcinoma',
+ 'vascular lesion',
+ 'nevus']
+
+severities = ["benign","malignant"]
 
     
 
@@ -25,9 +36,13 @@ def cancerClassification():
         x = load_img(image.filename,target_size=(224,224))
         x = np.expand_dims(img_to_array(x),0)
         #models to predict
-        # typee = model.predict(x)
-        # severity = model2.predict(x)
-        results.append({"type":"typee","severity":"severity"})
+        typee = typeCancerModel.predict(x)
+        severity = severityCancerModel.predict(x)
+        percentage = round(np.max(severity)*100,2)
+        print(percentage)
+        typee = types[np.argmax(typee)]
+        severity = severities[np.argmax(severity)]
+        results.append({"type":typee,"severity":severity,"percent":percentage})
         os.remove(image.filename)
 
 
@@ -35,4 +50,3 @@ def cancerClassification():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    # initModel("model.h5")
